466ea2a662d8a66153bd54afaaa9194015a883fc
hmughal71 <johnsamdough@gmail.com>
2021-12-22 19:39:42 +1100

    wrote initial code for js and completed form on html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,40 @@
 const todayEL = document.getElementById("currentDay")
-const today = moment().format("dddd,MMMM Do")
+const today = moment().format("dddd, MMMM Do, YYYY")
 console.log(today)
 todayEL.textContent = today
 
+const inputForm9 = $("#nineAm")
+console.log(inputForm9)
+inputForm9.on("click"       )
+
+const inputForm10 = $("#tenAm")
+console.log(inputForm10)
+inputForm10.on("click"     )
+
+const inputForm11 = $("#elevenAm")
+console.log(inputForm11)
+inputForm11.on("click"     )
+
+const inputForm12 = $("#twelvePm")
+console.log(inputForm12)
+inputForm12.on("click"     )
+
+const inputForm01 = $("#onePm")
+console.log(inputForm01)
+inputForm01.on("click"     )
+
+const inputForm02 = $("#twoPm")
+console.log(inputForm02)
+inputForm02.on("click"     )
+
+const inputForm03 = $("#threePm")
+console.log(inputForm03)
+inputForm03.on("click"     )
+
+const inputForm04 = $("#fourPm")
+console.log(inputForm04)
+inputForm04.on("click"     )
+
+const inputForm05 = $("#fivePm")
+console.log(inputForm05)
+inputForm05.on("click"     )
